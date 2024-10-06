11fdc71be8512f663cd20e04bb2792274d55b238
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-06 16:16:14 -0300

    feat: invoke seeder in program file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using dotnet_exam1.Src.Data;
+using dotnet_exam1.Src.Data.Seed;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
+    await Seeder.Seed(context);
 }
 
 app.Run();
