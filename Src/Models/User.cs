a0dea021deff1e756af2d27d099bfb4f396f606b
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-06 16:14:56 -0300

    fix: miss birthdate attribute

--- Src/Models/User.cs
+++ Src/Models/User.cs
@@ -6,10 +6,15 @@ namespace dotnet_exam1.Src.Models
     public class User
     {
         public int Id { get; set; }
+
         [Rut]
         public required string Rut { get; set; }
+
         [EmailAddress]
         public required string Email { get; set; }
+
+        [Birthdate]
+        public required DateOnly Birthdate { get; set; }
         public int GenderId { get; set; }
         public required Gender Gender { get; set; }
     }
