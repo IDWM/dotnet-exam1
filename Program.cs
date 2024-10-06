3a2fa78ce2f18788be0708b9b13ddad76c4860c5
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-06 17:54:44 -0300

    feat: add automapper and repositories

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using dotnet_exam1.Src.Data;
 using dotnet_exam1.Src.Data.Seed;
+using dotnet_exam1.Src.Interfaces;
+using dotnet_exam1.Src.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,10 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IGenderRepository, GenderRepository>();
 
 var app = builder.Build();
 
@@ -18,5 +24,6 @@ using (var scope = app.Services.CreateScope())
     await context.Database.MigrateAsync();
     await Seeder.Seed(context);
 }
+app.MapControllers();
 
 app.Run();
