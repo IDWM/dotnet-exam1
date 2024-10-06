16b6b2b20993165476fd936f46bbead63d2279ad
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-10-06 17:54:11 -0300

    feat: add base api controller

---
+++ Src/Controllers/BaseApiController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_exam1.Src.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase;
+}
